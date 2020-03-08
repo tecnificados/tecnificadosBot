e5e84e2a5fe32a73b12aff8683cacbf49ecf93ae
Juan Carlos Ballesteros Hermida <jcballesteroshermida@gmail.com>
2020-03-08 18:39:53 +0100

    Respuesta conseguida :D

--- src/main/java/org/tecnificados/com/bot/App.java
+++ src/main/java/org/tecnificados/com/bot/App.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 import java.util.Properties;
+import java.util.stream.Collectors;
 
 import org.apache.commons.io.FileUtils;
 import org.json.simple.JSONArray;
@@ -18,7 +19,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.tecnificados.com.bot.util.Messages;
 
+import twitter4j.Query;
+import twitter4j.QueryResult;
 import twitter4j.Status;
+import twitter4j.StatusUpdate;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
@@ -79,6 +83,26 @@ public class App
 	    return statusText;
 	}
 	
+	public static List<Status> searchtweets() throws TwitterException {
+		List<Status> resultados=new ArrayList<Status>();
+		if (twitter!=null)
+		{  
+	    
+	    Query query = new Query("@tecnificados");
+	    query.setSince("2020-01-01");
+	    QueryResult result = twitter.search(query);
+	     
+	    resultados = result.getTweets();
+	    /*
+	    resultados=result.getTweets().stream()
+	      .map(item -> item.getText())
+	      .collect(Collectors.toList());
+	      */
+		}
+		return resultados;
+		
+	}
+	
     public static void main( String[] args )
     {    	
     	Locale.setDefault(new Locale("es_ES"));
@@ -110,12 +134,37 @@ public class App
         
         log.info(Messages.getString("frases.sw")+" "+starWars.size()); 
         
+        /*
         try {
 			String createTweet = createTweet("Otro Tweet inicial automatizado de prueba");
 			log.info(createTweet);
         } catch (TwitterException e) {
 			log.error("Error twiteando:",e);
 		}
+		*/
+        
+        try {
+			List<Status> searchtweets = searchtweets();
+			for (Status s:searchtweets)
+			{
+				if (s.getText().contains("qué tal"))
+				{
+					//Asi se responde
+					//TODO externalizar a función y refactorizar a una clase estática todas las llamadas de Twitter
+					String userNick = s.getUser().getScreenName();
+					StatusUpdate stat= new StatusUpdate("Pasando el domingo @"+userNick);
+
+				    stat.setInReplyToStatusId(s.getId());
+				    
+				    twitter.updateStatus(stat);
+				}
+			}
+		} catch (TwitterException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
+        log.info("fin");
     }
 
 	
